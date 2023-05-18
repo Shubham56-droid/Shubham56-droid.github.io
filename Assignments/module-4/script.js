a713982b21044bddcd5bf07e35cea28f19bf8eff
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2023-05-18 22:31:04 +0530

    Add files via upload

--- Assignments/module-4/script.js
+++ Assignments/module-4/script.js
@@ -1,15 +1,24 @@
-(function(){
-    var namesList = ["Shubham","John","Jack","Ashutosh","Jai","Arpit","Ravi","Kinshuk"];
+(function () {
+  var namesList = [
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
 
-    for (let i = 0; i < namesList.length; i++) {
-        var startLetter = namesList[i].charAt(0).toLowerCase();
+  for (let i = 0; i < namesList.length; i++) {
+    var startLetter = namesList[i].charAt(0).toLowerCase();
 
-        if(startLetter == 'j'){
-            speakBye(namesList[i]);
-        }
-        else{
-            speakHello(namesList[i]);
-        }
-        
+    if (startLetter == "j") {
+      speakBye(namesList[i]);
+    } else {
+      speakHello(namesList[i]);
     }
+  }
 })();
